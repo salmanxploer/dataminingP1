98e273441d4a4ce429603064c573b9e014442411
Md Salman Hafiz <150608736+salmanxploer@users.noreply.github.com>
2026-06-06 18:32:47 +0600

    feat: Add clustering module initialization

---
+++ src/clustering/__init__.py
@@ -0,0 +1,10 @@
+"""
+Clustering Module
+KDD PHASE 5: Pattern Discovery
+
+Implements document clustering algorithms:
+- K-Means
+- HDBSCAN
+
+Objective: Group similar research papers into clusters
+"""
